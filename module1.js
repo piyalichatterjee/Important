cebc683ec5adc164f1f4d5446e584aece0eb275b
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2024-03-29 22:07:30 +0530

    Modules, cookies, localstorage, sessionstorage completed

---
+++ module1.js
@@ -0,0 +1,13 @@
+import module2 from "./module2.js" // je jinis default export hoi seta import er somoy jeokhono name dewa jai
+// import {cvv, salary} from "./module2.js" // default export hochena tokhon onno file e export er name diye ei file import hobe
+
+let val1 = "This is my value 1 variable"
+
+console.log(val1)
+console.log(module2)
+// console.log(cvv)
+// console.log(salary)
+module2()
+const myFunction = () => {
+
+}
---
+++ module2.js
@@ -0,0 +1,25 @@
+
+const val2 = "This is my value 2"
+const cvv = 123
+// export const salary = "30k"
+const salary = "30k"
+
+// export default val2 // jokhon ami export default krchi tokhon ami import er somoy jekono variable name dite pari
+
+// const test = "123435"
+// export default kono file e ektai kora somvob
+// export default  () => {
+//     console.log(val2)
+//     console.log(cvv)
+//     console.log(salary)
+// }
+
+const myFunction = () => {
+    console.log(val2)
+    console.log(cvv)
+    console.log(salary)
+}
+export default myFunction
+
+
+// export  { cvv, salary }
